843d98e447eb524f605a1dda2061f3738cc0b780
Rajat <raj87ng@gmail.com>
2021-09-02 23:23:45 +0530

    Spring project-1

---
+++ mockwebserver/src/test/java/com/rajat/mock/server/MockwebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajat.mock.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockwebserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-restclient/src/main/java/com/rajat/springrest/SpringRestClientwebserverApplication.java
@@ -0,0 +1,13 @@
+package com.rajat.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestClientwebserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestClientwebserverApplication.class, args);
+	}
+
+}
